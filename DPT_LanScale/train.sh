81968ecc661e762a50a80f58f76fa9317458a698
Ziyao Zeng <adonis.zzeng@gmail.com>
2024-03-14 22:54:09 -0400

    1st commit test

--- DPT_LanScale/train.sh
+++ DPT_LanScale/train.sh
@@ -1,4 +1,4 @@
-exp_name="0314_2_sigmoid_scale_no_init_larger_lr_tmux_2"
+exp_name="0314_2_test_tmux_0"
 
 mkdir ./models/${exp_name}
-CUDA_VISIBLE_DEVICES=2 python train.py configs/arguments_train_nyu.txt  2>&1 | tee ./models/${exp_name}/result.log
+CUDA_VISIBLE_DEVICES=0 python train.py configs/arguments_train_nyu.txt  2>&1 | tee ./models/${exp_name}/result.log
