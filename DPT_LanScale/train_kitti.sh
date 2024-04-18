f33ce8b14827000ba2f061ba4e451836a9049ffe
Ziyao Zeng <1147172798@qq.com>
2024-04-18 19:46:43 -0400

    update small format

--- DPT_LanScale/train_kitti.sh
+++ DPT_LanScale/train_kitti.sh
@@ -1,4 +1,4 @@
-exp_name="0416_kitti_exp_scale_tmux2"
+exp_name="0418_kitti_exp_scale_tmux3"
 
 mkdir ./models/${exp_name}
-CUDA_VISIBLE_DEVICES=2 python train.py configs/arguments_train_kittieigen.txt  2>&1 | tee ./models/${exp_name}/result.log
+CUDA_VISIBLE_DEVICES=3 python train.py configs/arguments_train_kittieigen.txt  2>&1 | tee ./models/${exp_name}/result.log
