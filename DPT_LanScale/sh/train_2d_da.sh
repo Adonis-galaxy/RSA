fbe5435664e47568498d0126ff85ddf9fe21fc90
Ziyao Zeng <adonis.zzeng@gmail.com>
2024-06-13 13:42:25 -0400

    file arrange

--- DPT_LanScale/sh/train_2d_da.sh
+++ DPT_LanScale/sh/train_2d_da.sh
@@ -1,4 +1,4 @@
-exp_name="0612_2d_da_tmux0"
+exp_name="0613_2d_da_tmux3"
 
 mkdir models/${exp_name}
-CUDA_VISIBLE_DEVICES=0 python train_2d.py configs/arguments_train_2d_da.txt  2>&1 | tee models/${exp_name}/result.log
+CUDA_VISIBLE_DEVICES=3 python train_2d.py configs/arguments_train_2d_da.txt  2>&1 | tee models/${exp_name}/result.log
