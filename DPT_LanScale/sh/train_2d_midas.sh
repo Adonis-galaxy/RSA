06314aa710d44db518374f032f0c96e417e51be7
Ziyao Zeng <zyzeng@eros.cs.yale.edu>
2024-07-10 14:14:29 -0400

    midas update

--- DPT_LanScale/sh/train_2d_midas.sh
+++ DPT_LanScale/sh/train_2d_midas.sh
@@ -1,4 +1,4 @@
-exp_name="0704_2d_midas_nyul09_tmux0"
+exp_name="0710_2d_midas_seg_txt_nyul09_tmux3"
 
 mkdir models/${exp_name}
-CUDA_VISIBLE_DEVICES=0 python train_2d.py configs/arguments_train_2d_midas.txt  2>&1 | tee models/${exp_name}/result.log
+CUDA_VISIBLE_DEVICES=3 python train_2d.py configs/arguments_train_2d_midas.txt  2>&1 | tee models/${exp_name}/result.log
