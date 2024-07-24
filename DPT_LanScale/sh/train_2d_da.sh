6b958ba1724a7db6431846418319d578b258ac26
Ziyao Zeng <adonis.zzeng@gmail.com>
2024-07-24 15:46:07 -0400

    update sh

--- DPT_LanScale/sh/train_2d_da.sh
+++ DPT_LanScale/sh/train_2d_da.sh
@@ -1,4 +1,4 @@
-exp_name="0724_2d_da_panoptic_seg_txt_normNl1_tmux0"
+exp_name="0724_2d_da_panoptic_seg_txt_NL1_tmux0"
 
 mkdir models/${exp_name}
 CUDA_VISIBLE_DEVICES=0 python train_2d.py configs/arguments_train_2d_da.txt  2>&1 | tee models/${exp_name}/result.log
--- DPT_LanScale/sh/train_2d_midas.sh
+++ DPT_LanScale/sh/train_2d_midas.sh
@@ -1,4 +1,4 @@
-exp_name="0718_2d_midas_seg_txt_norm_loss_Nl1_tmux0"
+exp_name="0724_2d_midas_panoptic_seg_txt_NL1_tmux0"
 
 mkdir models/${exp_name}
 CUDA_VISIBLE_DEVICES=0 python train_2d.py configs/arguments_train_2d_midas.txt  2>&1 | tee models/${exp_name}/result.log
