98867f0e28c177505eb4f3a1a2e23fa5fe155bde
Ziyao Zeng <adonis.zzeng@gmail.com>
2024-07-24 15:43:56 -0400

    update panoptic seg text

--- DPT_LanScale/sh/train_2d_da.sh
+++ DPT_LanScale/sh/train_2d_da.sh
@@ -1,4 +1,4 @@
-exp_name="0718_2d_da_seg_txt_norm_loss_l2_tmux2"
+exp_name="0724_2d_da_panoptic_seg_txt_normNl1_tmux0"
 
 mkdir models/${exp_name}
-CUDA_VISIBLE_DEVICES=2 python train_2d.py configs/arguments_train_2d_da.txt  2>&1 | tee models/${exp_name}/result.log
+CUDA_VISIBLE_DEVICES=0 python train_2d.py configs/arguments_train_2d_da.txt  2>&1 | tee models/${exp_name}/result.log
