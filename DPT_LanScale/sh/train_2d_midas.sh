a360518ffcb4c070648272f1aeba91eae1046761
Ziyao Zeng <zyzeng@eros.cs.yale.edu>
2024-07-31 23:39:28 -0400

    update sh

--- DPT_LanScale/sh/train_2d_midas.sh
+++ DPT_LanScale/sh/train_2d_midas.sh
@@ -1,4 +1,4 @@
-exp_name="0724_2d_midas_panoptic_seg_txt_Huber_NumberOnlyNoArea_tmux2"
+exp_name="0731_2d_midas_car_scale_01_001_tmux0"
 
 mkdir models/${exp_name}
-CUDA_VISIBLE_DEVICES=2 python train_2d.py configs/arguments_train_2d_midas.txt  2>&1 | tee models/${exp_name}/result.log
+CUDA_VISIBLE_DEVICES=0 python train_2d.py configs/arguments_train_2d_midas.txt  2>&1 | tee models/${exp_name}/result.log
